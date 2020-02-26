9f8ab26e155bb748520f474941606f7604c4fdd4
Zach White <zechnwhite@gmail.com>
2020-02-26 02:06:01 -0800

    adds files

---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const fs = require('fs');
+const AWS = require('aws-sdk');
+
+const app = express();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY
+});
+
+
+const testfile = {
+    path: './credentials.csv'
+}
+
+uploadFile(testfile, Math.random().toString().slice(14) + '.csv', 'zw-test1');
+//upload a file to s3 amazon when file is under 5mb. 
+function uploadFile(file, key, bucketname) {
+    
+
+    stream = fs.createReadStream(file.path);
+    
+    params = {
+        Bucket: bucketname,
+        Key: key,
+        Body: stream
+    }
+
+    s3.upload(params, function(err, data) {
+        if(err) {
+            console.log(err);
+        }
+
+        console.log('Successfully uploaded package. key:' + key);
+        console.log(data);
+    });
+
+}
+
+
+app.get('/showobjects', (req, res) => {
+    var params2 = {
+      Bucket: 'zw-test1'
+    };
+    s3.listObjects(params2, function(err, data) {
+      // ^^^^^aka, "show me all the items in this bucket"
+      if (err) console.log(err, err.stack);
+      // an error occurred
+      else console.log(data);
+    });
+})
+
+app.get('/s3Proxy', function(req, res, next) {
+  // download the file via aws s3 here
+  var fileKey = '81185.csv'
+
+  console.log('Trying to download file', fileKey);
+  var options = {
+    Bucket: 'zw-test1',
+    Key: fileKey
+  };
+
+  res.attachment(fileKey);
+  var fileStream = s3.getObject(options).createReadStream();
+  fileStream.pipe(res);
+});
+
+app.listen(5000);
